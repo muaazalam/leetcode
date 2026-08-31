e5fa6169b11192e6a56316d43e9c8ac3f49cb280
Muaaz Alam <76880894+muaazalam@users.noreply.github.com>
2026-08-31 02:56:12 -0500

    Time: 15 ms (43.65%), Space: 19.3 MB (94.8%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,18 +2,19 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s)!=len(t):
             return False
-        a={}
-        b={}
-        for n in s:
-            if n in a:
-                a[n]+=1
-            else:
-                a[n]=1
 
-        for m in t:
-            if m in b:
-                b[m]+=1
+        hash1={}
+        hash2={}
+        for n in s:
+            if n not in hash1:
+                hash1[n]=1
             else:
-                b[m]=1
-        return a==b
+                hash1[n]+=1
         
+        for m in t:
+            if m not in hash2:
+                hash2[m]=1
+            else:
+                hash2[m]+=1
+
+        return hash1==hash2
